b6d1b99d2cfcaf38076173dff3609e6f89807ca1
noboomta <puvana.jp36@gmail.com>
2020-12-19 13:05:33 +0700

    first function and class

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const myfirstelement = (
+  <span>
+    <h1>
+      Good morning {new Date().toLocaleTimeString("en-US").split(/:| /)}
+    </h1>
+  </span>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Someone() {
+  const everyone = ['John', 'See', 'Na'];
+  var anyone = everyone[Math.floor(Math.random() * everyone.length)];
+  return <span>and {anyone}</span>;
+}
+
+class Person extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: "first name",
+      lname: "last name",
+      age: 20
+    }
+  }
+
+  changeName = () => {
+    this.setState({
+      fname: "new fname",
+      lname: "new lname"
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello, my name is {this.state.fname} {this.state.lname} ({this.state.age})</p>
+        <button
+          type="button"
+          onClick={this.changeName}
+        >Change Name</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Person name="firstname" />, document.getElementById('root'));
